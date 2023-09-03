4d4014131aeb2d8860a7ee7e34d9fa0c8d00b4ce
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-09-03 23:01:16 +0300

    Added new component

---
+++ vk/src/app/test1/test1.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+//Создаем декоратор и присваиваем компоненте свойства
+@Component({
+  selector: 'app-test1', // название компоненты
+  templateUrl: './test1.component.html', // объявляем разметку
+  styleUrls: ['test1.component.css'] // объявляем стили
+})
+// Компонент не может существовать без модуля
+export class Test1Component {
+
+}
