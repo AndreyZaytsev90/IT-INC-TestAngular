d0bcc82cf8e47bc1e5dda5e3a13e01cde66d87bc
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-09-04 20:17:44 +0300

    Updated file

--- vk/src/app/app.module.ts
+++ vk/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import {Test1Component} from "./test1/test1.component";
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
+import { Test4Component } from './user/components/test4/test4.component';
+import { Test5Component } from './user/components/test5/test5.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Test1Component,
     Test2Component,
-    Test3Component // добавили наш компонент
+    Test3Component,
+    Test4Component,
+    Test5Component // добавили наш компонент
   ],
   imports: [
     BrowserModule,
