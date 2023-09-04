578351218b54f85dacc6a12cd40f0e5ab201e20a
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-09-04 22:45:06 +0300

    Updated file

--- vk/src/app/app.module.ts
+++ vk/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import {NgModule} from '@angular/core'
-import {BrowserModule} from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import {AppRoutingModule} from './app-routing.module'
-import {AppComponent} from './app.component'
-import {Test1Component} from './test1/test1.component'
-import {Test2Component} from './test2/test2.component'
-import {Test3Component} from './test3/test3.component'
-import {Test4Component} from './user/components/test4/test4.component'
-import {Test5Component} from './user/components/test5/test5.component'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { Test1Component } from './test1/test1.component'
+import { Test2Component } from './test2/test2.component'
+import { Test3Component } from './test3/test3.component'
+import { Test4Component } from './user/components/test4/test4.component'
+import { Test5Component } from './user/components/test5/test5.component'
 
 @NgModule({
   declarations: [
