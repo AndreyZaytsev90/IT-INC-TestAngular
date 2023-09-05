622fd4494cb85fc4b0ce98e109ccb83639621ca5
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-09-05 20:12:55 +0300

    Used NgModel for two-way binding

--- vk/src/app/app.component.ts
+++ vk/src/app/app.component.ts
@@ -1,44 +1,11 @@
 import { Component } from '@angular/core'
 
-interface IUser {
-  age: number
-  name: string
-}
-
 @Component({
   selector: 'vk-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'vk'
-  user: IUser = {
-    age: 33,
-    name: 'Andrey',
-  }
-  isAppLoading = true
-
-  textOne = ''
-  textTwo = 'start value'
-
-  constructor() {
-    setTimeout(() => {
-      this.isAppLoading = false
-    }, 3000)
-    setTimeout(() => {
-      this.user.age = 34
-    }, 4000)
-  }
-
-  onClickTitleHandler() {
-    this.title = 'MNITI'
-  }
-
-  onClickInputHandlerOne(event: Event) {
-    this.textOne = (event.currentTarget as HTMLInputElement).value
-  }
-
-  onClickInputHandlerTwo(event: Event) {
-    this.textTwo = (event.currentTarget as HTMLInputElement).value
-  }
+  appTitle = 'vk'
+  text = 'start value'
 }
