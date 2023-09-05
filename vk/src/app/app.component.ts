ed6c0058a01196cc2bb3e517167db792f1c94f68
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2023-09-05 11:57:40 +0300

    Updated file

--- vk/src/app/app.component.ts
+++ vk/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core'
 
+interface IUser {
+  age: number
+  name: string
+}
+
 @Component({
   selector: 'vk-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,33 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'vk'
+  user: IUser = {
+    age: 33,
+    name: 'Andrey',
+  }
+  isAppLoading = true
+
+  textOne: string | undefined
+  textTwo: string | undefined
+
+  constructor() {
+    setTimeout(() => {
+      this.isAppLoading = false
+    }, 3000)
+    setTimeout(() => {
+      this.user.age = 34
+    }, 4000)
+  }
+
+  onClickTitleHandler() {
+    this.title = 'MNITI'
+  }
+
+  onClickInputHandlerOne(event: Event) {
+    this.textOne = (event.currentTarget as HTMLInputElement).value
+  }
+
+  onClickInputHandlerTwo(event: Event) {
+    this.textTwo = (event.currentTarget as HTMLInputElement).value
+  }
 }
--- vk/src/app/test3/test3.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'vk-test3',
-
-  templateUrl: './test3.component.html',
-  styleUrls: ['./test3.component.scss'],
-})
-export class Test3Component {}
